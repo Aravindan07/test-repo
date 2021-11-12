932d921d48f2f3cae2495e9898717b802d2ac850
Aravindan <aravindanravi33@gmail.com>
2021-11-12 15:31:59 +0530

    hopefully it will work

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
 	res.send("Hello from Typescript and NodeJS!!!");
 });
 
-const port = process.env.port || 7000;
+const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
 	console.log("Server started at port 7000");
