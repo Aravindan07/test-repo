b338316667630d778107ebe3a7d46664cda86ff9
Aravindan <aravindanravi33@gmail.com>
2021-11-12 15:01:07 +0530

    test commit

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("Hello from Typescript and NodeJS");
+	res.send("Hello from Typescript and NodeJS!!!");
 });
 
 app.listen(7000, () => {
